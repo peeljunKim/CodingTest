66c5dad671c2a15c255221df95e788bbe68b46ae
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2026-01-04 20:21:51 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 96 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,11 @@
+/**
+해당 알고리즘은 입력 크기 n과 상관없이 return A[i](Code 1)를 항상 한 번만 수행하기 때문에 
+어떤 수가 들어와도 수행횟수가 1번이고, 수행시간은 O(1) 그래서 결과 값은 무조건 1 \n 0
+**/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
