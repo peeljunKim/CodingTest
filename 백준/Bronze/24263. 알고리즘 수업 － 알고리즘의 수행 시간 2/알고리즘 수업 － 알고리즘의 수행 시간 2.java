8e41305e74d144e0bf782d11177142d5a7d94bce
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2026-01-04 20:37:17 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n); 
+        System.out.println(1); 
+
+        sc.close();
+    }
+}
