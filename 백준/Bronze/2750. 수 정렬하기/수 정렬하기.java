ef4d6b30efbee458dc6e80a84cf55be913e07fae
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2026-01-09 18:59:19 +0900

    [Bronze II] Title: 수 정렬하기, Time: 232 ms, Memory: 20768 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr); 
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.print(sb.toString());
+        sc.close();
+    }
+}
