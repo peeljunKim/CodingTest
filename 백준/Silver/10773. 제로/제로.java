8a9f74224a5563e657c893a6388427784a34ba9a
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2026-01-27 21:45:55 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 23480 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        long sum = 0;
+
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        for (int value : stack) {
+            sum += value;
+        }
+
+        System.out.println(sum);
+    }
+}
