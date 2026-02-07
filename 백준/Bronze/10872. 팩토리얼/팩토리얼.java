0708b395f05a67a35d56b47a654d19f411bff7f4
peeljunKim <135033836+peeljunKim@users.noreply.github.com>
2026-02-07 19:22:57 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long result = 1; 
+        
+        for (int i = 1; i <= N; i++) {
+            result *= i;
+        }
+        
+        System.out.println(result);
+    }
+}
